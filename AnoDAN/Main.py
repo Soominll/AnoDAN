e50f8850bcd0ba0bf3489b80a80d07a41d207343
Soo Min Lee <lsm46147570@gmail.com>
2023-04-13 00:07:00 +0900

    Update Main.py

--- AnoDAN/Main.py
+++ AnoDAN/Main.py
@@ -42,7 +42,6 @@ if gpus:
     for g in gpus:
         tf.config.experimental.set_memory_growth(g, True)
   except RuntimeError as e:
-    # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
     print(e)
 
 mirrored_strategy = tf.distribute.MirroredStrategy()
